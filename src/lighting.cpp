5db4ca875dda2f269e88db1541782e9da8b95736
sarath-menon <sarathmenon.downey@gmail.com>
2022-01-07 09:59:29 +0530

    better documentation

--- src/lighting.cpp
+++ src/lighting.cpp
@@ -1,11 +1,15 @@
 #include "ogre_demo.h"
 
-void MyTestApp::setup_lighting(SceneManager *scene, SceneNode *root_node){
+void MyTestApp::setup_lighting(SceneManager *scene, SceneNode *root_node) {
 
-    // without light we would just get a black screen
+  // set the ambient lighting
+  scene->setAmbientLight(ColourValue{0.1, 0.5, 0.5});
+
+  // without light we would just get a black screen
   Light *light = scene->createLight("MainLight");
   SceneNode *light_node = root_node->createChildSceneNode();
+
+  // attach light to node in scene
   light_node->setPosition(20, 80, 50);
   light_node->attachObject(light);
-
 }
--- src/scene.cpp
+++ src/scene.cpp
@@ -3,7 +3,7 @@
 SceneManager* MyTestApp::setup_scene(){
 
   SceneManager *scene = getRoot()->createSceneManager();
-  scene->setAmbientLight(ColourValue{0.5, 0.5, 0.5});
+ 
 
   // register our scene with the RTSS
   RTShader::ShaderGenerator *shadergen =
